c6da4460069d952798c7f10f93d350f42025fd1c
Angelita Santos <angelitasantos@gmail.com>
2024-05-31 09:41:59 -0300

    escritores

---
+++ static/js/quemsoueu.js
@@ -0,0 +1,57 @@
+
+/* alexiahetka */
+let alexiahetka = document.querySelector('#alexiahetka');
+let selectorAlexia = `De Curitiba/PR, Aléxia é escritora de ficção, roteirista e redatora publicitária.
+    Tem dois livros físicos e alguns ebooks publicados, de ficção e técnica para escritores.
+    Quando não está correndo atrás do filho ou pensando no que comer, gosta de ver filmes e séries e discutir sobre.`;
+if (alexiahetka) {alexiahetka.textContent = selectorAlexia};
+
+/* andypereira */
+let andypereira = document.querySelector('#andypereira');
+let selectorAndy = `Filho de Atena, rêveur, nerd, KatyCat, carioca, arte-educador, arquiteto, universalista, oraculista,
+    metido a fotógrafo e escritor. — Muita coisa em um só ENFP cheio de aleatoriedades na caixola e sempre cheio de coisas
+    pra falar com quem quer ouvir (ou ler)!`;
+if (andypereira) {andypereira.textContent = selectorAndy};
+
+/* angelitasantos */
+let angelitasantos = document.querySelector('#angelitasantos');
+let selectorAngelita = `Mineira de Beagá, administradora, escritora de ficção, fotógrafa e viajante. Gosta de ouvir músicas
+    e histórias, de assistir os mais variados esportes e de jogar board games. Autora de literatura sáfica:
+    Livro: O perigo muda de endereço, publicado em novembro de 2019;
+    Conto: Tão correto e tão bonito, publicado em 2020 na Antologia: Corações coloridos que pulsam e no site wattpad.com.`;
+if (angelitasantos) {angelitasantos.textContent = selectorAngelita};
+
+/* danisalemme */
+let danisalemme = document.querySelector('#danisalemme');
+let selectorDani = `Escritora independente, autora dos romances sáficos: Sentimento Secreto, Amor Dividido, Prazer, Maria Eduarda
+    e outros contos publicados por aí. Paulistana de nascimento, Andreense de coração, mãe de quatro gatos e dois cachorros,
+    cervejeira, apaixonada por instrumentos musicais e cinema.`;
+if (danisalemme) {danisalemme.textContent = selectorDani};
+
+/* giuferreira */
+let giuferreira = document.querySelector('#giuferreira');
+let selectorGiu = `É bissexual, não-binário, carioca da Zona Oeste e professor no Município de Queimados.
+    Começou a escrever com fanfics de Eragon e hoje, aos 25, acumula alguns títulos publicados,
+    como a coleção de fantasia, “Uma história de primavera” e o romance sáfico “Só as Paredes Sabem”,
+    parte da série O B de LGBT da Se Liga Editorial. Além de outras histórias gratuitas no Wattpad.
+    Assina o prefácio da primeira antologia do Coletivo Litera e participa do segundo volume com o contro
+    “Hormônios e crises nem-tão-mais-adolescentes-assim no 301”.`;
+if (giuferreira) {giuferreira.textContent = selectorGiu};
+
+/* carolcoelho */
+let carolcoelho = document.querySelector('#carolcoelho');
+let selectorCarol = `Mistura de paulista com sul-mato-grossense. Gosta de escrever um bom drama psicológico
+    Young Adult. É autora do livro "Anamnese". Atuou na revisão da antologia Edifício Litera.`;
+if (carolcoelho) {carolcoelho.textContent = selectorCarol};
+
+/* oceanoalbuquerque */
+let oceanoalbuquerque = document.querySelector('#oceanoalbuquerque');
+let selectorOceano = `Natural de Recife/PE, publicou seu primeiro livro em 2018, "O mundo novo de Tommy", além de acumular
+    participações em antologias como 21 contos assombrados. No Edifício Litera escreveu o conto "A drag queen do 406".`;
+if (oceanoalbuquerque) {oceanoalbuquerque.textContent = selectorOceano};
+
+/* pipparivera */
+let pipparivera = document.querySelector('#pipparivera');
+let selectorPippa = `Nascida em Brasília/DF, escreve desde criança e tem diversas obras publicadas. É autora de
+    "As vidas de um amor", "Estranho", "Lua Cheia" e outros títulos. Na Antologia Edifício Litera escreveu "O traidor do 701".`;
+if (pipparivera) {pipparivera.textContent = selectorPippa};
