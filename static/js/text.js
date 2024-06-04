14ba586e630776daeecca7bccbc4d924ad1656ca
Angelita Santos <angelitasantos@gmail.com>
2024-06-04 09:40:43 -0300

    index

--- static/js/text.js
+++ static/js/text.js
@@ -6,13 +6,13 @@ if (fullName) {fullName.textContent = selectorName};
 
 
 let nameFunction = document.querySelector('#name-function');
-let selectorNameFunction = 'Escritores Brasileiros Independentes';
+let selectorNameFunction = 'Escritores LGBTQIAP+ Independentes';
 if (nameFunction) {nameFunction.textContent = selectorNameFunction};
 
 
 let textoGeral = document.querySelector('#texto-geral');
 let selectorTexto = `O Coletivo Litera foi fundado em 2020 e reune autores
-                        independentes LGBTQIA+, sendo um espaço de acolhimento, apoio e troca de experiências.
+                        independentes LGBTQIAP+, sendo um espaço de acolhimento, apoio e troca de experiências.
                         Divulgamos projetos, eventos literários, concursos, trocamos
                         ideias e dicas. Apoiamos nossos autores, divulgando seus
                         livros, incentivando a escrita e o lançamento de novos projetos.
@@ -20,6 +20,6 @@ let selectorTexto = `O Coletivo Litera foi fundado em 2020 e reune autores
                         com contos de 9 autores do nosso Coletivo. E temos diversos projetos por vir.
                         Estamos espalhados por todo o Brasil, valorizamos a
                         diversidade e respeitamos as diferenças. Somos um grupo sem fins
-                        lucrativos, que busca ser um ambiente de apoio a literatura independente LGBTQIA +.`;
+                        lucrativos, que busca ser um ambiente de apoio a literatura independente LGBTQIAP+.`;
 if (textoGeral) {textoGeral.textContent = selectorTexto};
 
